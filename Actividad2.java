3efb2c0f6a85feafbb1638f35c16d1178fd667a4
JoaquinHaedo <jhaedo2000@gmail.com>
2025-02-01 15:00:57 -0300

    Create Actividad2.java

---
+++ Actividad2.java
@@ -0,0 +1,16 @@
+public class Actividad2 {
+    public static void main(String[] args) {
+        int edad = 24;
+        double altura = 1.75;
+        char inicial = 'J';
+        String ciudad = "Ciudad de Buenos Aires";
+
+        int sumaEdad = edad + 5;
+        double alturaPorDos = altura * 2;
+        String mensajeInicial = "Tu inicial es " + inicial;
+
+        System.out.println("La suma de tu edad con 5 es: " + sumaEdad);
+        System.out.println("El doble de tu altura es: " + alturaPorDos + " metros");
+        System.out.println(mensajeInicial);
+    }
+}
