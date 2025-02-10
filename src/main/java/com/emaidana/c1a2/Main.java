49e0e7abfc36750e23375d885171ee85431a062a
Emmanuel <emma.maidana@gmail.com>
2025-02-09 23:31:05 -0300

    clase 1 actividad 2

---
+++ src/main/java/com/emaidana/c1a2/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int edad = 34;
+        double altura = 1.65;
+        char inicial = 'E';
+        String ciudad = "Buenos Aires";
+
+        int nuevaEdad = edad + 5;
+        double dobleAltura = altura * 2;
+        String mensaje = "Tu inicial es " + inicial;
+
+        System.out.println("Nueva edad: " + nuevaEdad);
+        System.out.println("Altura multiplicada por 2: " + dobleAltura);
+        System.out.println(mensaje);
+    }
+}
